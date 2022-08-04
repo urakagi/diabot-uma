251b767129fbbc0e932da09a4ffb61abb9506100
Romulus Urakagi Tsai <urakagi@gmail.com>
2022-08-04 23:16:43 +0800

    Add shortcuts and gametora maps

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,26 +1,76 @@
-function parseCommand(content) {
-    if (content === '美妙') {
-        return '非速智愛帶30美妙直接讓一張卡，養出辣雞馬怪我囉？';
-    } else if (content === '上課') {
-        return '你這個問題在我們的學程影片可以得到解答：' +
-            'https://www.youtube.com/playlist?list=PL_BTNAzC0OXjiUsM58XTfkNJloTFUdxpo';
-    } else if (content === '補課') {
-        return '你這個問題在我們的學程影片可以得到解答：' +
-            'https://www.youtube.com/playlist?list=PL_BTNAzC0OXjiUsM58XTfkNJloTFUdxpo';
-    } else if (content === '歷戰') {
-        return '2022年了還有人歷戰你認真的嗎\nhttps://www.youtube.com/watch?v=CFHjRCUYEAw';
+const { query } = require('../libs/db');
+const ADMIN_IDS = ['191744769540620288'];
+
+async function parseShortcuts(word) {
+    const sql = `SELECT content FROM shortcuts WHERE word='${word}'`;
+    const res = await query(sql);
+    if (res.length > 0) {
+        return res[Math.floor(res.length * Math.random())].content;
+    } else {
+        return `無此指令：${word}`;
     }
+}
+
+async function addShortcuts(line) {
+    const word = line.split(' ')[0];
+    const content = line.substring(word.length + 1);
+    await query(`INSERT INTO shortcuts (word, content) VALUES ('${word}', '${content}')`);
+    return `成功加入${word}。`;
+}
+
+async function deleteShortcuts(word) {
+    await query(`DELETE FROM shortcuts WHERE word='${word}'`);
+    return `刪除${word}成功。`;
+}
+
+async function sendGameTora(page) {
+    const sql = `SELECT url FROM gametora WHERE page='${page}'`;
+    const res = await query(sql);
+    if (res.length > 0) {
+        return res[Math.floor(res.length * Math.random())].url;
+    } else {
+        return `尚無該頁面資料：${page}`;
+    }
+}
+
+async function addGameTora(input) {
+    const page = input.replaceAll('　', ' ').split(' ')[0];
+    const url = input.substring(page.length + 1);
+    await query(`INSERT INTO gametora (page, url) VALUES ('${page}', '${url}')`);
+    return `已加入頁面資料：${page}`;
 }
 
 module.exports = {
     name: 'messageCreate',
     async execute(message) {
-        const content = message.content;
-        if (content.startsWith('!') || content.startsWith('！')) {
-            const resp = parseCommand(content.substring(1));
+        try {
+            const content = message.content;
+            let resp;
+            if (content.startsWith('!gt ')) {
+                resp = await sendGameTora(content.substring(4));
+            } else if (content.startsWith('！虎 ') || content.startsWith('!虎 ')) {
+                resp = await sendGameTora(content.substring(3));
+            } else if (content.startsWith('+gt ')) {
+                resp = await addGameTora(content.substring(4));
+            } else if (content.startsWith('＋虎 ') || content.startsWith('+虎 ')) {
+                resp = await addGameTora(content.substring(3));
+            } else if (content.startsWith('+!') || content.startsWith('＋！') || content.startsWith('+！')) {
+                if (ADMIN_IDS.includes(message.author.id)) {
+                    resp = await addShortcuts(content.substring(2));
+                }
+            } else if (content.startsWith('-!')) {
+                if (ADMIN_IDS.includes(message.author.id)) {
+                    resp = await deleteShortcuts(content.substring(2));
+                }
+            } else if (content.startsWith('!') || content.startsWith('！')) {
+                resp = await parseShortcuts(content.substring(1));
+            }
             if (resp) {
                 await message.channel.send(resp);
             }
+        } catch (e) {
+            console.log(e);
+            await message.channel.send(`發生錯誤：${e}`);
         }
     },
 };
--- libs/db.js
+++ libs/db.js
@@ -7,5 +7,12 @@ db.run('CREATE TABLE IF NOT EXISTS videos (' +
     '"id" INTEGER PRIMARY KEY AUTOINCREMENT, ' +
     '"keyword" TEXT, ' +
     '"url" TEXT)');
+db.run('CREATE TABLE IF NOT EXISTS shortcuts (' +
+    '"id" INTEGER PRIMARY KEY AUTOINCREMENT, ' +
+    '"word" TEXT, ' +
+    '"content" TEXT)');
+db.run('CREATE TABLE IF NOT EXISTS gametora (' +
+    '"page" TEXT PRIMARY KEY ON CONFLICT REPLACE, ' +
+    '"url" TEXT)');
 
 module.exports = { db, query };
